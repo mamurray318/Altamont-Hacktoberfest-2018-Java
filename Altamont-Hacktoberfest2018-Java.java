cb8ae89d93f00511650202cf84e97d678559f644
Unknown <sgidiere@gmail.com>
2018-10-15 16:55:34 -0500

    l33t H@x0r

--- Altamont-Hacktoberfest2018-Java.java
+++ Altamont-Hacktoberfest2018-Java.java
@@ -2,10 +2,11 @@ public static void main( String args[] ) {
    System.out.println("Hello, my name is Hampton!");
    // add your name and print statement below
    System.out.prinln("Hello, my name is David!");
-   
+
    System.out.println("Hello, this is Henry");
-   
-   
-   
-   
+
+   System.out.println("H3ll0 my n@m3 is Bud | @m @ l33t H@x0r");
+
+
+
 }
--- hacktoberfest.java
+++ hacktoberfest.java
@@ -3,11 +3,11 @@ public class hactoberfest {
       System.out.println("Hello, my name is Hampton!");
       // add your name and print statement below
       System.out.println("Hey there, it's Mr. James!");
-      
+
       System.out.println("Hello. This is Henry. I want to sleep");
-   
-   
-   
-   
+
+      System.out.println("H3ll0 my n@m3 is Bud | @m @ l33t H@x0r");
+
+
    }
 }
