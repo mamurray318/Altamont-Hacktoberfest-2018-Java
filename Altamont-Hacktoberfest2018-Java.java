5de65151cb93fd0dbdc76ea0ddbdd23012d9d49f
GrandWarlock7 <38705981+GrandWarlock7@users.noreply.github.com>
2018-10-15 17:07:42 -0500

    Pizza

--- Altamont-Hacktoberfest2018-Java.java
+++ Altamont-Hacktoberfest2018-Java.java
@@ -16,4 +16,5 @@ public static void main( String args[] ) {
    System.out.println("Hello, this is Mrs. Williams");
    
    system.out.println("Hello, this is Eddy!");
+   system.out.println("Hello, this is a person who wants pizza very much!");
 }
