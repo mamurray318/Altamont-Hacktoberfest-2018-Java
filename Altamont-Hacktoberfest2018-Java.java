b64db775f53c00bde41714be614fd25fe713e0cd
Hampton Walker <43421101+hamptonwalker01@users.noreply.github.com>
2018-10-15 16:53:55 -0500

    Update Altamont-Hacktoberfest2018-Java.java

--- Altamont-Hacktoberfest2018-Java.java
+++ Altamont-Hacktoberfest2018-Java.java
@@ -1,7 +1,7 @@
 public static void main( String args[] ) {
    System.out.println("Hello, my name is Hampton!");
    // add your name and print statement below
-   System.out.prinln("Hello, my name is David!");
+   System.out.println("Hello, my name is David!");
    
    System.out.println("Hello, this is Henry");
    
